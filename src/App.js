ef0e2b5a13c0278aee64649410b38950cce70d6d
Zsofia Matyasi <zsofia.matyasi@gmail.com>
2021-08-31 11:22:54 +0200

    feat: get data with axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [state, setState] = useState({
+    boardgames: [ 
+    ]
+  });
+
+  useEffect(() => {
+    axios.get('https://bgg-json.azurewebsites.net/collection/edwalter')
+    .then(res => {console.log(res.data)});
+    // .then(res => setState({boardgames: res.data}));
+  }, [])
+
   return (
     <div className="App">
-      
+      <Router>
+      <div className="App">
+        <div className="container">
+          <Route path="/" render ={props => (
+            <>
+              <div className="wrapper">
+          {/* <BoardgamesList boardgames = {state.boardgames}/> */}
+            </div>
+            </>
+          )}/>
+        </div>
+      </div>
+      </Router>
     </div>
   );
 }
