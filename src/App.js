5d825f43e1493811185e5689abaeded8f37bc68b
csilla-hegedus <hegeduscsilla333@gmail.com>
2021-08-31 11:41:07 +0200

    feat: add header and navbar to application

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Header from './components/layout/Header';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="container">
+      <Header />
     </div>
   );
 }
---
+++ src/components/layout/Header.js
@@ -0,0 +1,10 @@
+import Navbar from "./Navbar";
+
+export default function Header() {
+    return (
+        <div className="header">
+        <h1><a href="/">React To Games</a></h1>
+        <Navbar />
+        </div>
+    );
+}
