d1785fb7878d7a3480bf40ee888bc4da4eceb7d3
csilla-hegedus <hegeduscsilla333@gmail.com>
2021-08-31 13:45:35 +0200

    feat: create route for game details

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
 import Header from './components/layout/Header';
+import Details from './components/GameDetail';
 
 import './App.css';
 
 function App() {
   return (
     <div className="container">
-      <Header />
+      <Router>
+        <Route path="/">
+          <Header />
+        </Route>
+        <Route path="/details">
+          <Details />
+        </Route>
+      </Router>
     </div>
   );
 }
