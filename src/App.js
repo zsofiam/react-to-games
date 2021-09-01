0b161ee56aeae59cad29dcbf0126e9dd6dee2b20
csilla-hegedus <hegeduscsilla333@gmail.com>
2021-09-01 10:59:13 +0200

    refactor: relocate boardgames state to BoardGamesList.js from App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import React, { useState, useEffect } from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
-import axios from 'axios';
 
 import Header from './components/layout/Header';
 import GameDetail from './components/GameDetail';
@@ -11,37 +9,20 @@ import './App.css';
 
 
 const App = () => {
-  console.log("App function start");
-  const [state, setState] = useState({
-    boardgames: [ 
-    ]
-  });
-
-  useEffect(() => {
-    axios.get('https://bgg-json.azurewebsites.net/collection/edwalter')
-    .then(res => {
-      setState({boardgames: res.data});
-      console.log("App function useEffect");
-    });
-  }, [])
-
-
   return (
     <div className="container">
       <FavouritesProvider>
         <Router>
           <Header />
-          <Route exact path="/" render ={props => (
-                <>
+          <Route exact path="/">
                   <div className="wrapper">
-              <BoardgamesList boardgames = {state.boardgames}/>
+              <BoardgamesList allBoardgames = {true}/>
                 </div>
-                </>
-              )}/>
+              </Route>
           <Route path="/boardgame/:gameId" component={GameDetail}/>
           <Route path="/favourites">
             <div className="wrapper">
-              <BoardgamesList boardgames={[]}/>
+              <BoardgamesList/>
             </div>
           </Route>
         </Router>
--- src/components/BoardgamesList.js
+++ src/components/BoardgamesList.js
@@ -1,22 +1,35 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import Boardgame from "./Boardgame";
 import { FavouritesContext } from "./FavouritesContext";
+import axios from 'axios';
 
 const BoardgamesList = (props) => {
     const favourites = useContext(FavouritesContext);
     console.log("Favourites: ", favourites);
+    const [state, setState] = useState({
+        boardgames: [ 
+        ]
+      });
+    
+    useEffect(() => {
+    axios.get('https://bgg-json.azurewebsites.net/collection/edwalter')
+    .then(res => {
+        setState({boardgames: res.data});
+        console.log("App function useEffect");
+    });
+    }, [])
 
      return (
          <>
-         {props.boardgames
+         {props.allBoardgames
          ?
         
-        props.boardgames.map((boardgame, index) => (
-            <Boardgame key={index} id={index + 1} boardgame = {boardgame}/>
+        state.boardgames.map((boardgame) => (
+            <Boardgame key={boardgame.gameId} id={boardgame.gameId} boardgame = {boardgame}/>
         ))
        
         :
-        favourites.map((favourite) => (
+        favourites[0].map((favourite) => (
             <Boardgame key={favourite.gameId} id={favourite.gameId} boardgame = {favourite}/>
             ))
         }
