f78718eda358047d2502aa955c21f75bd0081f56
csilla-hegedus <hegeduscsilla333@gmail.com>
2021-09-01 10:57:32 +0200

    refactor: use rating instead of bggRating

--- src/components/FavouritesContext.js
+++ src/components/FavouritesContext.js
@@ -6,17 +6,17 @@ export const FavouritesProvider = props => {
     console.log("FavouritesProvider function start");
     const [favouriteGames, setFavouriteGames] = useState([
         {
-            gameId: 333,
+            gameId: 333333,
             name: "Test Game",
             image: "https://images.unsplash.com/photo-1611371805429-8b5c1b2c34ba?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
-            bggRating: 8.3333,
+            rating: 8.3333,
             yearPublished: 2020
         },
         {
             gameId: 55555,
             name: "Test Game 2",
             image: "https://images.unsplash.com/photo-1530328411047-7063dbd29029?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80",
-            bggRating: 7.123,
+            rating: 7.123,
             yearPublished: 2021
         }
     ]);
