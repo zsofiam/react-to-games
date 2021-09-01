de3d0f133dd1bd9e7e7c170c972a4e0a7bf2e032
csilla-hegedus <hegeduscsilla333@gmail.com>
2021-09-02 00:37:21 +0200

    fix: eliminate errors when adding games to favourites state

--- src/components/FavouritesContext.js
+++ src/components/FavouritesContext.js
@@ -4,21 +4,26 @@ import React, { useState, createContext } from 'react';
 export const FavouritesContext = createContext();
 
 export const FavouritesProvider = (props) => {
-    let [favouriteGames, setfavouriteGames] = useState(JSON.parse(localStorage.getItem("favourites")) || []);
+    let [favouriteGames, setFavouriteGames] = useState(JSON.parse(localStorage.getItem("favourites")) || []);
     const toggleFavouriteGames = (gameId) => {
-        //favouriteGames = localStorage.getItem("favourites");
-        const newFavouriteGames = favouriteGames.filter(favourite => favourite.gameId !== gameId);
-        if (favouriteGames.length === newFavouriteGames.length){
-            axios.get(`https://bgg-json.azurewebsites.net/thing/${gameId}?callback=myCallback`)
-            .then(res => favouriteGames.push(res.data))
+        if (favouriteGames.length === 0){
+            axios.get(`https://bgg-json.azurewebsites.net/thing/${gameId}`)
+            .then(res => setFavouriteGames(favouriteGames => [...favouriteGames, res.data]))
             .then(localStorage.setItem("favourites", JSON.stringify(favouriteGames)));
-            
         } else {
-           favouriteGames = setfavouriteGames([...newFavouriteGames]);
-           localStorage.setItem("favourites", favouriteGames);
+            favouriteGames = JSON.parse(localStorage.getItem("favourites"));
+            const newFavouriteGames = favouriteGames.filter(favourite => favourite.gameId !== gameId);
+            if (favouriteGames.length === newFavouriteGames.length){
+                axios.get(`https://bgg-json.azurewebsites.net/thing/${gameId}`)
+                .then(res => setFavouriteGames(favouriteGames => [...favouriteGames, res.data]))
+                .then(localStorage.setItem("favourites", JSON.stringify(favouriteGames)));
+                
+            } else {
+            favouriteGames = setFavouriteGames(newFavouriteGames => [...newFavouriteGames]);
+            localStorage.setItem("favourites", favouriteGames);
+            }
         }
         console.log(favouriteGames);
-        console.log("2:", localStorage.getItem("favourites"), JSON.parse(localStorage.getItem("favourites"))[0]);
     }
 
     return (
