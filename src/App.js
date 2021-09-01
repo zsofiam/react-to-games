d4b4cf7b2d4e970b37ce6df51891c3f9061ffea3
csilla-hegedus <hegeduscsilla333@gmail.com>
2021-09-01 10:16:41 +0200

    feat: [WIP] use context for favourites

--- src/App.js
+++ src/App.js
@@ -5,59 +5,47 @@ import axios from 'axios';
 import Header from './components/layout/Header';
 import GameDetail from './components/GameDetail';
 import BoardgamesList from './components/BoardgamesList';
+import { FavouritesProvider } from './components/FavouritesContext';
 
 import './App.css';
 
 
 const App = () => {
+  console.log("App function start");
   const [state, setState] = useState({
     boardgames: [ 
     ]
   });
-  const [favouriteGames, setFavouriteGames] = useState(
-    [
-      {
-          gameId: 333,
-          name: "Test Game",
-          image: "https://images.unsplash.com/photo-1611371805429-8b5c1b2c34ba?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
-          rating: 8.3333,
-          yearPublished: 2020
-      },
-      {
-          gameId: 55555,
-          name: "Test Game 2",
-          image: "https://images.unsplash.com/photo-1530328411047-7063dbd29029?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80",
-          rating: 7.123,
-          yearPublished: 2021
-      }
-    ]);
 
   useEffect(() => {
     axios.get('https://bgg-json.azurewebsites.net/collection/edwalter')
-    .then(res => setState({boardgames: res.data}));
+    .then(res => {
+      setState({boardgames: res.data});
+      console.log("App function useEffect");
+    });
   }, [])
 
 
   return (
     <div className="container">
-      <Router>
-        <Header />
-        <Route exact path="/" render ={props => (
-              <>
-                <div className="wrapper">
-            <BoardgamesList boardgames = {state.boardgames}/>
-              </div>
-              </>
-            )}/>
-        <Route path="/boardgame/:gameId" component={GameDetail}/>
-        <Route path="/favourites" render ={props => (
-              <>
-                <div className="wrapper">
-            <BoardgamesList boardgames = {favouriteGames}/>
-              </div>
-              </>
-            )}/>
-      </Router>
+      <FavouritesProvider>
+        <Router>
+          <Header />
+          <Route exact path="/" render ={props => (
+                <>
+                  <div className="wrapper">
+              <BoardgamesList boardgames = {state.boardgames}/>
+                </div>
+                </>
+              )}/>
+          <Route path="/boardgame/:gameId" component={GameDetail}/>
+          <Route path="/favourites">
+            <div className="wrapper">
+              <BoardgamesList boardgames={[]}/>
+            </div>
+          </Route>
+        </Router>
+      </FavouritesProvider>
     </div>
 
   );
--- src/components/BoardgamesList.js
+++ src/components/BoardgamesList.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
 import Boardgame from "./Boardgame";
+import { FavouritesContext } from "./FavouritesContext";
 
 const BoardgamesList = (props) => {
+    const favourites = useContext(FavouritesContext);
+    console.log("Favourites: ", favourites);
 
-     return props.boardgames.map((boardgame, index) => (
-        <Boardgame key={index} id={index + 1} boardgame = {boardgame}/>
-    ));
-    
+     return (
+         <>
+         {props.boardgames
+         ?
+        
+        props.boardgames.map((boardgame, index) => (
+            <Boardgame key={index} id={index + 1} boardgame = {boardgame}/>
+        ))
+       
+        :
+        favourites.map((favourite) => (
+            <Boardgame key={favourite.gameId} id={favourite.gameId} boardgame = {favourite}/>
+            ))
+        }
+        </>
+     )
 }
 
 export default BoardgamesList;
