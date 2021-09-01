c32d1bb7af15f8da2ffc03687491cb1fb818ec23
csilla-hegedus <hegeduscsilla333@gmail.com>
2021-09-01 11:36:06 +0200

    feat: add favourites to context from homepage

--- src/components/Boardgame.js
+++ src/components/Boardgame.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from "react-router-dom";
 import axios from 'axios';
 
+import { FavouritesContext } from "./FavouritesContext";
 import './Boardgame.css';
 
 const Boardgame = (props) => {
+    const favourites = useContext(FavouritesContext);
     const addToFavourites = (gameId) => {
         axios.get(`https://bgg-json.azurewebsites.net/thing/${gameId}`)
-        .then(res => console.log(res.data));
+        .then(res => favourites[1]([...favourites[0], res.data]));
     }
     
     const{name, gameId, image, rating, yearPublished} = props.boardgame;
