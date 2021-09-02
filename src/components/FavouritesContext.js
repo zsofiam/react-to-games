9bdb616f6c3383f36774b2e805869c6e4b7f6447
csilla-hegedus <hegeduscsilla333@gmail.com>
2021-09-02 10:14:07 +0200

    fix: [WIP] refactor favourites setter to be able to store in localstorage

--- src/components/FavouritesContext.js
+++ src/components/FavouritesContext.js
@@ -4,18 +4,28 @@ import React, { useState, createContext } from 'react';
 export const FavouritesContext = createContext();
 
 export const FavouritesProvider = (props) => {
+    console.log("before toggle");
     const [favouriteGames, setFavouriteGames] = useState(JSON.parse(localStorage.getItem("favourites")) || []);
+    console.log("before toggle", favouriteGames);
     const toggleFavouriteGames = (gameId) => {
         if (favouriteGames.length === 0){
+            console.log("if")
             axios.get(`https://bgg-json.azurewebsites.net/thing/${gameId}`)
-            .then(res => setFavouriteGames(favouriteGames => [...favouriteGames, res.data]))
+            .then(res => {
+                const copyOfFavourites = [...favouriteGames, res.data];
+                setFavouriteGames(copyOfFavourites);
+            })
             .then(localStorage.setItem("favourites", JSON.stringify(favouriteGames)));
         } else {
+            console.log("else");
             setFavouriteGames(JSON.parse(localStorage.getItem("favourites")));
             const newFavouriteGames = favouriteGames.filter(favourite => favourite.gameId !== gameId);
             if (favouriteGames.length === newFavouriteGames.length){
                 axios.get(`https://bgg-json.azurewebsites.net/thing/${gameId}`)
-                .then(res => setFavouriteGames(favouriteGames => [...favouriteGames, res.data]))
+                .then(res => {
+                    const copyOfFavourites = [...favouriteGames, res.data];
+                    setFavouriteGames(copyOfFavourites);
+                })
                 .then(localStorage.setItem("favourites", JSON.stringify(favouriteGames)));
                 
             } else {
@@ -23,7 +33,7 @@ export const FavouritesProvider = (props) => {
             localStorage.setItem("favourites", favouriteGames);
             }
         }
-        console.log(favouriteGames);
+        console.log("after toggle", favouriteGames);
     }
 
     return (
