3530e033dfea5d9ca2b355bb76a6519d50254abb
csilla-hegedus <hegeduscsilla333@gmail.com>
2021-09-02 09:53:04 +0200

    refactor: make favouriteGames collection constant using its setter

--- src/components/FavouritesContext.js
+++ src/components/FavouritesContext.js
@@ -4,14 +4,14 @@ import React, { useState, createContext } from 'react';
 export const FavouritesContext = createContext();
 
 export const FavouritesProvider = (props) => {
-    let [favouriteGames, setFavouriteGames] = useState(JSON.parse(localStorage.getItem("favourites")) || []);
+    const [favouriteGames, setFavouriteGames] = useState(JSON.parse(localStorage.getItem("favourites")) || []);
     const toggleFavouriteGames = (gameId) => {
         if (favouriteGames.length === 0){
             axios.get(`https://bgg-json.azurewebsites.net/thing/${gameId}`)
             .then(res => setFavouriteGames(favouriteGames => [...favouriteGames, res.data]))
             .then(localStorage.setItem("favourites", JSON.stringify(favouriteGames)));
         } else {
-            favouriteGames = JSON.parse(localStorage.getItem("favourites"));
+            setFavouriteGames(JSON.parse(localStorage.getItem("favourites")));
             const newFavouriteGames = favouriteGames.filter(favourite => favourite.gameId !== gameId);
             if (favouriteGames.length === newFavouriteGames.length){
                 axios.get(`https://bgg-json.azurewebsites.net/thing/${gameId}`)
@@ -19,7 +19,7 @@ export const FavouritesProvider = (props) => {
                 .then(localStorage.setItem("favourites", JSON.stringify(favouriteGames)));
                 
             } else {
-            favouriteGames = setFavouriteGames(newFavouriteGames => [...newFavouriteGames]);
+            setFavouriteGames(newFavouriteGames => [...newFavouriteGames]);
             localStorage.setItem("favourites", favouriteGames);
             }
         }
