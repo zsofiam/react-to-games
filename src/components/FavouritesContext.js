0fa7c5af5432bed48d2a46cb48b5853e85a5b5d6
csilla-hegedus <hegeduscsilla333@gmail.com>
2021-09-02 20:11:44 +0200

    fix: [WIP] add and remove items to state variable avoiding localstorage

--- src/components/FavouritesContext.js
+++ src/components/FavouritesContext.js
@@ -1,19 +1,22 @@
 import axios from 'axios';
-import React, { useState, createContext } from 'react';
+import React, { useState, createContext, useEffect } from 'react';
 
 export const FavouritesContext = createContext();
 
 export const FavouritesProvider = (props) => {
-    const [favouriteGames, setFavouriteGames] = useState(JSON.parse(localStorage.getItem("favourites")) || []);
+    let [favouriteGames, setFavouriteGames] = useState(JSON.parse(localStorage.getItem("favourites")));
     const toggleFavouriteGames = (gameId) => {
+        if (favouriteGames === null) {
+            favouriteGames = [];
+        }
         setFavouriteGames(JSON.parse(localStorage.getItem("favourites")));
         // if favourites has no element add new boardgame
         if (favouriteGames.length === 0){
             axios.get(`https://bgg-json.azurewebsites.net/thing/${gameId}`)
             .then(res => {
-                setFavouriteGames([...favouriteGames, res.data]);
+                favouriteGames.push(res.data);
             })
-            .then(localStorage.setItem("favourites", JSON.stringify(favouriteGames)));
+            //.then(localStorage.setItem("favourites", JSON.stringify(favouriteGames)));
         // else check if game is in array
         } else {
             const newFavouriteGames = favouriteGames.filter(favourite => favourite.gameId !== gameId);
@@ -21,17 +24,21 @@ export const FavouritesProvider = (props) => {
             if (favouriteGames.length === newFavouriteGames.length){
                 axios.get(`https://bgg-json.azurewebsites.net/thing/${gameId}`)
                 .then(res => {
-                    setFavouriteGames([...favouriteGames, res.data]);
+                    favouriteGames.push(res.data);
                 })
-                .then(localStorage.setItem("favourites", JSON.stringify(favouriteGames)));
+                //.then(localStorage.setItem("favourites", JSON.stringify(favouriteGames)));
             // else gameId is found, game should be removed
             } else {
-            setFavouriteGames([...newFavouriteGames]);
-            localStorage.setItem("favourites", JSON.stringify(favouriteGames));
+            favouriteGames = [...newFavouriteGames];
+            //localStorage.setItem("favourites", JSON.stringify(favouriteGames));
             }
         }
         console.log(favouriteGames);
     }
+
+    useEffect(() => {
+        localStorage.setItem("favourites", JSON.stringify(favouriteGames));
+    }, [favouriteGames])
 
     return (
        <FavouritesContext.Provider  value={[favouriteGames, toggleFavouriteGames]}>{props.children}</FavouritesContext.Provider>
